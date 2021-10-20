aa844f415a24cab427fd530ac37dfe79a8f64076
Alice Moretti <ali.moret88@gmail.com>
2021-10-20 12:00:51 +0300

    Added hero component

---
+++ src/components/Hero.js
@@ -0,0 +1,85 @@
+import React from "react"
+import styled from "styled-components"
+import { StaticImage } from "gatsby-plugin-image"
+import { MdKeyboardArrowDown } from "react-icons/md"
+
+const Hero = () => {
+  return (
+    <Wrapper>
+      <StaticImage
+        src="../assets/images/roses2.jpg"
+        className="header-pic"
+        alt="white roses"
+        placeholder="blurred"
+      />
+
+      <div className="hero-content">
+        <h1 className="hero-title">
+          Marta&nbsp;
+          <span style={{ color: "hsla(0, 0%, 77%, 0.65)" }}>&amp;</span>
+          &nbsp;Alberto
+        </h1>
+      </div>
+      <div className="arrow-container">
+        <MdKeyboardArrowDown className="arrow-down" />
+      </div>
+    </Wrapper>
+  )
+}
+
+const Wrapper = styled.header`
+  //on smartphone and tablet the hero is not full height. Fromm 900px width onwards it takes the full height.
+  position: relative;
+  width: 100vw;
+  height: 100vh;
+  max-height: 900px;
+  position: absolute;
+  top: 0;
+  left: 0;
+
+  .header-pic {
+    width: 100%;
+    height: 100%;
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-position: center;
+    filter: brightness(0.6);
+  }
+
+  .hero-content {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .arrow-container {
+    position: absolute;
+    bottom: 50px;
+    width: 100%;
+    text-align: center;
+  }
+
+  .arrow-container svg {
+    color: #ffff;
+    font-size: 4rem;
+    animation: mymove 2s ease-out infinite;
+    position: relative;
+  }
+
+  @keyframes mymove {
+    from {
+      top: 0px;
+    }
+    to {
+      top: 30px;
+    }
+  }
+`
+
+export default Hero
