d184d9dc29995048a927b70d9b62eb94880b2335
Alice Moretti <ali.moret88@gmail.com>
2021-10-20 16:09:50 +0300

    removed About component from hp

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,14 +2,12 @@ import React from "react"
 import Hero from "../components/Hero"
 import Layout from "../components/Layout"
 import Title from "../components/Title"
-import About from "../components/About"
 
 export default function Home() {
   return (
     <Layout>
       <Hero />
       <Title />
-      <About />
     </Layout>
   )
 }
