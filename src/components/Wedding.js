bf5bab2140af975d3dcfbb8ddfd194696f3aba63
Alice Moretti <ali.moret88@gmail.com>
2021-10-27 13:26:30 +0300

    added onMouseleave and onMouseEnter for icons and made the component responsive

--- src/components/Wedding.js
+++ src/components/Wedding.js
@@ -26,29 +26,19 @@ export const query = graphql`
       titolo
       sottotitolo
     }
-    allContentfulProgramma {
-      nodes {
-        orario
-        titolo
-        icona {
-          internal {
-            content
-          }
-        }
-      }
-    }
   }
 `
 
 const Wedding = () => {
   const data = useStaticQuery(query)
-  const programmi = data.allContentfulProgramma.nodes
-  console.log(programmi)
 
   let animationContainer1 = createRef()
   let animationContainer2 = createRef()
   let animationContainer3 = createRef()
-  console.log(animationContainer1, animationContainer2, animationContainer3)
+
+  let heartPlay = null
+  let winePlay = null
+  let partyPlay = null
 
   //useEffect runs the function whenever there is a re-render: initially when the component first loads
   //and whenever the data change
@@ -58,7 +48,7 @@ const Wedding = () => {
       animationData: heart, //animation file
       renderer: "svg",
       loop: true,
-      autoplay: true,
+      autoplay: false,
     }
 
     let wineIcon = {
@@ -66,42 +56,49 @@ const Wedding = () => {
       animationData: wine, //animation file
       renderer: "svg",
       loop: true,
-      autoplay: true,
+      autoplay: false,
     }
     let partyIcon = {
       container: animationContainer3.current,
       animationData: party, //animation file
       renderer: "svg",
       loop: true,
-      autoplay: true,
+      autoplay: false,
     }
-    /*
-    lottie.loadAnimation({
-      container: animationContainer.current,
-    
-      animationData: icon, //animation file
-      renderer: "svg",
-      loop: true,
-      autoplay: true,
-    })
-    */
 
     //lottie.loadAnimation can only take one argument, the following sintax would render ONLY the first icon:
     // lottie.loadAnimation(heartIcon,wineIcon, partyIcon )
-    let heartAnimation = lottie.loadAnimation(heartIcon)
-    let wineAnimation = lottie.loadAnimation(wineIcon)
-    let partyAnimation = lottie.loadAnimation(partyIcon)
+    heartPlay = lottie.loadAnimation(heartIcon)
+    winePlay = lottie.loadAnimation(wineIcon)
+    partyPlay = lottie.loadAnimation(partyIcon)
   }, [])
 
-  /*
-  function handleStop() {
-    heart1.pause()
+  function startHeartAnimation() {
+    heartPlay.play()
+    heartPlay.setSpeed(2)
   }
 
-  function handleStart() {
-    heart1.play()
+  function stopHeartAnimation() {
+    //animation goes to frame 0 (value) and then stops. goToAndStop(value, isFrame)
+    heartPlay.goToAndStop(0, true)
   }
-  */
+
+  function startWineAnimation() {
+    winePlay.play()
+    winePlay.setSpeed(2)
+  }
+
+  function stopWineAnimation() {
+    winePlay.goToAndStop(0, true)
+  }
+
+  function startPartyAnimation() {
+    partyPlay.play()
+  }
+
+  function stopPartyAnimation() {
+    partyPlay.goToAndStop(0, true)
+  }
 
   return (
     <Wrapper>
@@ -109,22 +106,6 @@ const Wedding = () => {
         <h3>{data.contentfulMatrimonio.titolo}</h3>
         <p>{data.contentfulMatrimonio.sottotitolo}</p>
       </div>
-      {/*
-      <div className="program-style">
-        {programmi.map(programma => {
-          return (
-            <div className="single-program-style">
-              <div
-                className="animation-container"
-                ref={animationContainer}
-              ></div>
-              <p>{programma.orario}</p>
-              <p>{programma.titolo}</p>
-            </div>
-          )
-        })}
-      </div>
-      */}
       <div className="program-container">
         <StaticImage
           src="../assets/images/spots.png"
@@ -132,20 +113,32 @@ const Wedding = () => {
           layout="constrained"
           className="background-style"
         />
-        <div className="program-card">
+        <div
+          className="program-card"
+          onMouseEnter={startHeartAnimation}
+          onMouseLeave={stopHeartAnimation}
+        >
           <div className="animation-container" ref={animationContainer1}></div>
           <p className="time-style">ore 17:00</p>
-          <p>cerimonia</p>
+          <p className="text-style">cerimonia</p>
         </div>
-        <div className="program-card">
+        <div
+          className="program-card"
+          onMouseEnter={startWineAnimation}
+          onMouseLeave={stopWineAnimation}
+        >
           <div className="animation-container" ref={animationContainer2}></div>
           <p className="time-style">ore 19:00</p>
-          <p>cena</p>
+          <p className="text-style">cena</p>
         </div>
-        <div className="program-card">
+        <div
+          className="program-card"
+          onMouseEnter={startPartyAnimation}
+          onMouseLeave={stopPartyAnimation}
+        >
           <div className="animation-container" ref={animationContainer3}></div>
           <p className="time-style">ore 22:00</p>
-          <p>tutti si balla</p>
+          <p className="text-style">tutti si balla</p>
         </div>
       </div>
     </Wrapper>
@@ -162,6 +155,7 @@ const Wrapper = styled.section`
 
   .program-container {
     width: 70vw;
+    max-width: 1270px;
     display: grid;
     grid-template-columns: repeat(3, 1fr);
     column-gap: 1.5rem;
@@ -177,22 +171,13 @@ const Wrapper = styled.section`
     opacity: 0.3;
   }
   .program-card {
+    width: 100%;
     text-align: center;
+    cursor: pointer;
   }
   .animation-container {
-    width: 80px;
+    width: 100px;
     margin: 0 auto;
-  }
-
-  .program-style {
-    display: grid;
-    grid-template-columns: repeat(3, 1fr);
-  }
-
-  .single-program-style {
-    display: flex;
-    flex-direction: column;
-    display: inline-block;
   }
 
   .time-style {
@@ -200,6 +185,23 @@ const Wrapper = styled.section`
     color: var(--dark-pink);
     font-weight: 500;
     font-size: 0.8rem;
+    text-decoration: underline;
+  }
+
+  .text-style {
+    text-transform: uppercase;
+    font-size: 0.8rem;
+    font-family: Spectral;
+    font-weight: 700;
+  }
+
+  @media (max-width: 700px) {
+    .program-container {
+      grid-template-columns: 1fr;
+    }
+    .program-card {
+      margin-bottom: 1rem;
+    }
   }
 `
 
