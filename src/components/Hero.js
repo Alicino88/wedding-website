e51b238330846c3c70e0db7a65a52aab46969b30
Alice Moretti <ali.moret88@gmail.com>
2021-11-03 10:59:38 +0200

    Udate to gero pic

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -7,7 +7,7 @@ const Hero = () => {
   return (
     <Wrapper>
       <StaticImage
-        src="../assets/images/roses5.jpg"
+        src="../assets/images/roses8.jpeg"
         className="header-pic"
         alt="white roses"
         placeholder="blurred"
@@ -41,7 +41,7 @@ const Wrapper = styled.header`
     background-repeat: no-repeat;
     background-size: cover;
     background-position: center;
-    filter: brightness(0.5);
+    filter: brightness(0.7);
   }
 
   .hero-content {
