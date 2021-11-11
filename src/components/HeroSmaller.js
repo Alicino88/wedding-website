daea897b39663e0f939d09eff5f9cd50a3e2141b
Alice Moretti <ali.moret88@gmail.com>
2021-11-11 15:19:09 +0200

    Added HeroSmaller component

---
+++ src/components/HeroSmaller.js
@@ -0,0 +1,52 @@
+import React from "react"
+import styled from "styled-components"
+import { StaticImage } from "gatsby-plugin-image"
+
+const HeroSmaller = () => {
+  return (
+    <Wrapper>
+      <StaticImage
+        src="../assets/images/beach.jpg"
+        className="header-pic"
+        alt="seychelles beach"
+        placeholder="blurred"
+      />
+
+      <div className="hero-content">
+        <h1 className="hero-title">Fateci sognare</h1>
+      </div>
+    </Wrapper>
+  )
+}
+
+const Wrapper = styled.header`
+  width: 100vw;
+  height: 70vh;
+  max-height: 900px;
+  position: absolute;
+  top: 0;
+  left: 0;
+
+  .header-pic {
+    width: 100%;
+    height: 100%;
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-position: center;
+    filter: brightness(0.6);
+  }
+
+  .hero-content {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+`
+
+export default HeroSmaller
