e8e525e9ba4731344c5a9bbde33deb81aa479d0b
Alice Moretti <ali.moret88@gmail.com>
2021-11-12 12:32:26 +0200

    Removed extra white space from navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -14,7 +14,6 @@ const Navbar = ({ toggle }) => {
         </button>
         <button className="menu-btn">
           <Link className="link-container" to="/#il-matrimonio">
-            {" "}
             il matrimonio
           </Link>
         </button>
