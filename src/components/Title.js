ca8a4d4845c56152476ad6784533391c447a3bff
Alice Moretti <ali.moret88@gmail.com>
2021-11-12 11:42:41 +0200

    Added margin-bottom undr h1 and increased padding

--- src/components/Title.js
+++ src/components/Title.js
@@ -13,10 +13,11 @@ const Wrapper = styled.section`
   margin-top: 90vh;
   width: 100%;
   text-align: center;
-  padding: 2.2rem 1rem 4rem 1rem;
+  padding: 2.2rem 1.2rem 4rem 1.2rem;
 
   .title-style {
     color: var(--dark-grey);
+    margin-bottom: 15px;
   }
 
   //Since the hero image max-height is 900px, above this height there needs to be a fixed distance from the menu bar and the Title component.
